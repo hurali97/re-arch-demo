25e6d81568c87717bc942559c8cd0c271ab67c6b
Hur Ali <hurali97@gmail.com>
2022-04-13 00:21:49 +0500

    implemented NativeDlogSpec in Dlog.java

--- android/app/src/main/java/com/rearchdemo/custommodules/Dlog.java
+++ android/app/src/main/java/com/rearchdemo/custommodules/Dlog.java
@@ -1,4 +1,17 @@
 package com.rearchdemo.custommodules;
+import com.facebook.react.bridge.ReactApplicationContext;
+import android.util.Log;
 
-public class Dlog {
+public class Dlog extends NativeDlogSpec {
+    private static final String TAG = "Dlog";
+    public static final String NAME = "Dlog";
+
+    public Dlog(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public void log(String message) {
+        Log.d(TAG, message);
+    }
 }
