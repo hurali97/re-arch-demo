b8e0200d631abc048d8f485f014547aa628455b6
hurali97 <hurali97@gmail.com>
2022-07-20 02:24:05 +0500

    fix: updated spec file

--- rn-app/src/custom-modules/specs/NativeDlog.ts
+++ rn-app/src/custom-modules/specs/NativeDlog.ts
@@ -2,7 +2,7 @@ import type {TurboModule} from 'react-native';
 import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
-  log(message: string): Promise<string>;
+  log(message: string): void;
 }
 
 export default TurboModuleRegistry.get<Spec>('Dlog');
